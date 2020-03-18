ba522114207a4b07c8e381cdf49d7df608b9ca1b
Adam Spencer <aspencer0919@gmail.com>
2020-03-18 19:51:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,14 +51,6 @@ def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
-def play(board)
-  moves = 1
-  until moves == 10
-    turn(board)
-    moves += 1
-  end
-end
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -111,5 +103,13 @@ end
 def winner(board)
   if won?(board)
     return board[won?(board)[0]]
+  end
+end
+
+def play(board)
+  moves = 1
+  until moves == 10
+    turn(board)
+    moves += 1
   end
 end
