fb317860024f42aea7b2f5e8bf35f39d2b12d50f
Adam Spencer <aspencer0919@gmail.com>
2020-03-18 19:35:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,6 +37,20 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |spaces|
+    if spaces == "X" || spaces == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
 def play(board)
   moves = 1
   until moves == 10
